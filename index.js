c8130731bb5d3a347184c7140f5946cac9e1ee91
sanjiro <defconx00@gmail.com>
2023-11-16 17:02:23 -0500

    findbyid [x]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./api/server');
 
 const port = 9000;
 server.listen(port, () => {
-    console.log('server test')
+    console.log('listening on port', port)
 })
-// START YOUR SERVER HERE
+
 console.log("hey you")
